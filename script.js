fa59486ef5a53fe9e3a8e67c6fe3017f99108319
vedang-123 <buchakevedang97@gmail.com>
2023-12-14 20:32:37 +0530

    ADD INTIAL FILES

---
+++ script.js
@@ -0,0 +1,13 @@
+const bar=document.getElementById('bar');
+const nav=document.getElementById('navbar');
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+const close=document.getElementById('close');
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active')
+    })
+}
